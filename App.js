ea97d75a7beb9750ae9302afc9a80da1db54e5bd
Sharique Akhter <60446567+sharique850@users.noreply.github.com>
2023-03-07 00:20:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+// import TextForm from './Components/TextForm';
+import About from "./Components/About";
+
+function App() {
+  return (
+    <>
+      {/* <Navbar title = "My website" about = "About me"/>
+    <div className="container my-3">
+    <TextForm title = "Enter your text here" />
+      </div>  */}
+
+      <About />
+    </>
+  );
+}
+
+export default App;
